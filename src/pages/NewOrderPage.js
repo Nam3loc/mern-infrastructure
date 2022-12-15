6307f8059e575ac03106ba54da74ad149e464004
Coleman Escue <99232536+Nam3loc@users.noreply.github.com>
2022-12-15 13:30:09 -0500

    saved token in local storage

---
+++ src/pages/NewOrderPage.js
@@ -0,0 +1,5 @@
+export default function NewOrderPage() {
+    return (
+        <h1>This is the New Order Page</h1>
+    )
+}
---
+++ src/pages/OrderHistoryPage.js
@@ -0,0 +1,5 @@
+export default function OrderHistoryPage() {
+    return (
+        <h1>This is the Order History Page</h1>
+    )
+}
