240b413a27b0a47d5546a7145358784db2b69441
Coleman Escue <99232536+Nam3loc@users.noreply.github.com>
2022-12-15 16:05:41 -0500

    finished log out, login in pages

--- src/App.js
+++ src/App.js
@@ -5,23 +5,24 @@ import NewOrderPage from './pages/NewOrderPage';
 import OrderHistoryPage from './pages/OrderHistoryPage';
 import NavBar from './components/NavBar';
 import { Routes, Route } from 'react-router-dom';
+import { getUser } from './utilities/users-service';
 
 function App() {
-  const [user, setUser] = useState(null); // need falsey value - can be false, 0, or ''
+  const [user, setUser] = useState(getUser()); // need falsey value - can be false, 0, or ''
 
   return (
     <main className='App'>
       {
         user ? 
         <>
-          <NavBar />
+          <NavBar getName={user.name} setUser={setUser} />
           <Routes>
             <Route path='/orders/new' element={<NewOrderPage />} />
             <Route path='/orders' element={<OrderHistoryPage />} />
           </Routes>
         </>
         :
-          <AuthPage />
+          <AuthPage setUser={setUser} />
       }
     </main>
   );
